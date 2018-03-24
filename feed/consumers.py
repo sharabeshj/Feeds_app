e27ff5924885e5afd36a332cc1d4e07f22f8367e
sharabesh <sharabeshjayaraman@gmail.com>
2018-03-25 04:28:44 +0530

    notification added

--- feed/consumers.py
+++ feed/consumers.py
@@ -6,37 +6,38 @@ class FeedConsumer(AsyncWebsocketConsumer):
 		self.user = self.scope["user"]
 		if self.user.is_active:
 			await self.channel_layer.group_add("users",self.channel_name)
-			await self.channel_layer.group_send("users",{
+			''''await self.channel_layer.group_send("users",{
 				'type' : 'feed_message',
 				'text' : json.dumps({
 					'username' : self.user.username,
 					'is_logged_in' : True
 					})
-				})
+				})'''
 			await self.accept()
 		else:
 			await self.close()
 
 	async def disconnect(self,close_code):
 		await self.channel_layer.group_discard("users",self.channel_name)
-		await self.channel_layer.group_send("users",{
+		'''await self.channel_layer.group_send("users",{
 			'type' : 'feed_message',
 			'text' : json.dumps({
 				'username' : self.user.username,
 				'is_logged_in' : False
 				})
-			})
+			})'''
 
-	async def recieve(self,text_data):
+	async def receive(self,text_data):
 		text_data_json = json.loads(text_data)
 		message = text_data_json['message']
 		user = text_data_json['user']
+		activity_type = text_data_json['activity_type']
 		await self.channel_layer.group_send("users",{
 			'type' : 'feed_message',
 			'text' : json.dumps({
 				'user' : user,
 				'message' : message,
-				'has_posted' : True
+				'activity_type' : activity_type
 			})
 		})
 
